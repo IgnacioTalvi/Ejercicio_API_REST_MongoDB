81106bc6b52dd21fccb5773e5aa5bf6369022d1a
Ignacio Talvi <ignaciotalvi@Ignacios-MacBook-Pro.local>
2025-05-11 14:39:49 +0200

    add/create-provider-functionality

--- controllers/providers.controller.js
+++ controllers/providers.controller.js
@@ -9,4 +9,26 @@ async function getAllProviders(req, res) {
   }
 }
 
-module.exports = { getAllProviders };
+async function createProvider(req, res) {
+  try {
+    const { company_name, CIF, address, url_web } = req.body;
+
+    const newProvider = new Provider({
+      company_name,
+      CIF,
+      address,
+      url_web,
+    });
+
+    const savedProvider = await newProvider.save();
+
+    res.status(201).json({
+      message: "Proveedor creado",
+      provider: savedProvider,
+    });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
+
+module.exports = { getAllProviders, createProvider };
--- routes/providers.routes.js
+++ routes/providers.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const providersController = require("../controllers/providers.controller");
 
 router.get("/", providersController.getAllProviders);
+router.post("/", providersController.createProvider);
 
 module.exports = router;

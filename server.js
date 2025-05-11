0b7370af5927a5c85686aaf2adcac98167948202
Ignacio Talvi <ignaciotalvi@Ignacios-MacBook-Pro.local>
2025-05-11 15:23:33 +0200

    add/getAllProducts

--- server.js
+++ server.js
@@ -1,30 +1,32 @@
-const express = require("express")
-const mongoose = require("./config/db_mongo")
+const express = require("express");
+const mongoose = require("./config/db_mongo");
 const app = express();
 const port = 3000;
 
 // Middlewares
-const morgan = require('./middlewares/morgan');
+const morgan = require("./middlewares/morgan");
 
 // Logger
-app.use(morgan(':method :host :status :param[id] - :response-time ms :body'));
+app.use(morgan(":method :host :status :param[id] - :response-time ms :body"));
 
-//Rutas 
+//Rutas
 // const productsRoutes = require('./routes/products.routes');
-const providersRoutes = require('./routes/providers.routes');
+const providersRoutes = require("./routes/providers.routes");
+const productsRoutes = require("./routes/providers.routes");
 
 app.use(express.json()); // Habilito recepción de JSON en servidor
 
 //API
 // app.use('/api/products',productsRoutes);
-app.use('/api/providers',providersRoutes);
+app.use("/api/providers", providersRoutes);
+app.use("/api/products", productsRoutes);
 
 //Ruta de prueba
 app.get("/", (req, res) => {
-    res.send(`Servidor funcionando en ${port}`)
-})
+  res.send(`Servidor funcionando en ${port}`);
+});
 
 //Arrancamos el servidor
 app.listen(port, () => {
-    console.log(`Servidor arrancado en http://localhost:${port}`)
-})
+  console.log(`Servidor arrancado en http://localhost:${port}`);
+});
